b0b7915078e1426e264dafd82dc0d832761949ea
Ayush <112577569+Ayus444@users.noreply.github.com>
2024-09-03 15:08:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7388897263:AAHoIBnDg1tU0wAcgx2kxjy5iKFRGWm9YgM")
+    API_ID =int(os.environ["API_ID", 13794728
+    ]
+    API_HASH = os.environ["API_HASH", "25c1a5b45b981d601832905e220e16c7"]
+    AUTH_USERS = "7134052961"
+    AUTH_GROUP = "-1002246964527"""
